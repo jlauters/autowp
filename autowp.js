40f71d2ab0327ce8fcc2fb66875d72f3ea262f34
Jon Lauters <jonathan.lauters@gmail.com>
2015-06-10 15:00:37 -0600

    reworked code to replace outdated dependencies. Created package.json

--- autowp.js
+++ autowp.js
@@ -1,19 +1,44 @@
 // module dependencies
-var http     = require('http');
-var request  = require('request');
-var url      = require('url');
-var AdmZip   = require('adm-zip');
-var fs       = require('fs');
-var nconf    = require('nconf');
-var mysql    = require('mysql');
-var execSync = require('exec-sync');
+var http          = require('http'),
+    request       = require('request'),
+    path          = require('path'),
+    url           = require('url'),
+    fs            = require('fs'),
+    targz         = require('tar.gz'),
+    nconf         = require('nconf'),
+    mysql         = require('mysql'),
+    child_process = require('child_process'); 
+
+global.appRoot = path.resolve(__dirname);
+
+// Native Node.js execSync
+function execSync(command) {
+
+    console.log('in execSync. Command: ');
+    console.log(command);
+
+    // Run passed in command in subshell
+    child_process.exec(command + ' 2>&1 1>output && echo done! > done');
+
+    // Block the event loop until this command has executed.
+    while(!fs.existsSync('done')) {
+        // do nothing
+    }
+
+    // Read output
+    var output = fs.readFileSync('output');
+
+    // Delete temp files
+    fs.unlinkSync('output');
+    fs.unlinkSync('done');
+
+    return output;
+}
 
 // load our config from file
 nconf.use('file', {file: 'config.json'});
-//nconf.load();
-//nconf.save();
 
-// create empty mysql database
+// mysql connection string
 var db_conn = mysql.createConnection({
     host: nconf.get('database:host'),
     user: nconf.get('database:user'),
@@ -21,21 +46,23 @@ var db_conn = mysql.createConnection({
     port: nconf.get('database:port')
 });
 
-db_conn.connect(function(err) {
-    if(err) throw err;
-});
-
-db_conn.query('CREATE DATABASE ' + nconf.get('db_name'), function(err, results) {
-    if (err) throw err;
+// Mysql Connect. Create WP Shell DB. Disconnect
+db_conn.connect(function(err) { if(err) throw err; });
+db_conn.query('CREATE DATABASE IF NOT EXISTS ' + nconf.get('db_name'), function(err, results) { 
+    if(err) { 
+        throw err;
+    } else { 
+        console.log('... database %s created', nconf.get('db_name'));
+    }
 });
 db_conn.end();
-
-console.log('.. database %s created', nconf.get('db_name'));
 
 // this callback is for events you wish to occur
 // after the wordpress zip file has been downloaded
 // unzipped and moved into the webroot directory
 var download_callback = function() {
+
+   console.log('in download Callback ...');
 
    // rename our wordpress directory to the new application name we want
    fs.rename(nconf.get('webroot') + 'wordpress', nconf.get('webroot') + nconf.get('app_name'), function() {
@@ -93,6 +120,34 @@ var download_callback = function() {
        execSync("cd /Applications/MAMP/bin/; sudo ./startApache.sh");
        console.log('... webserver bounce complete.');
    }, 5000);
+
+
+   // All Done.
+   console.log('done with zipdownload function');
+   console.log('autowp script complete ...');
+
+}
+
+
+// zip file download, mv, inflate
+function zipDownload(zipurl, zip_location, callback) {
+
+    
+    var r = request.get(zipurl).pipe(fs.createWriteStream('latest.tar.gz'));
+    r.on("finish", function() {
+        console.log('file to uncompress: ' + appRoot + '/latest.tar.gz');
+     
+        var compressed_file = appRoot + '/latest.tar.gz';
+        var dest = zip_location;
+
+        var uncompress = new targz().extract(compressed_file, dest, function(err) {
+            if(err) { console.log("Extraction Error: " + err); }
+            else {
+                console.log('wordpress extraction to: ' + zip_location + nconf.get('app_name') + ' complete ...');
+                callback();
+            }
+        });
+    });
 }
 
 // download wordpress
@@ -101,45 +156,5 @@ var download_callback = function() {
 // a callback function of items after download.
 var unzip_result = zipDownload(nconf.get('zipfile_url'), nconf.get('webroot'), download_callback);
 
-// zip file download, mv, inflate
-function zipDownload(zipurl, zip_location, callback) {
 
-    var options = {
-        host: url.parse(zipurl).host,
-        path: url.parse(zipurl).pathname,
-        port: 80
-    };
-
-    http.get(options, function(response) {
-
-        var data = [];
-        var dataLen = 0;
-
-        response.on('data', function(chunk) {
-            data.push(chunk);
-            dataLen += chunk.length;
-        }).on('end', function() {
-            console.log('zip download has been completed.');
-
-            var buf = new Buffer(dataLen);
-            for(var i=0, len = data.length, pos = 0; i < len; i++) {
-                data[i].copy(buf, pos);
-                pos += data[i].length;
-            }
-
-            var zip = new AdmZip(buf);
-            var zipEntries = zip.getEntries();
-
-            zip.extractAllTo(zip_location, true);
-
-            console.log('zip extraction to' + zip_location + ' complete ..');
-
-            // optional callback after zip extract
-            callback();
-        });
-    });
-}
-
-console.log('done with zipdownload function');
-console.log('autowp script complete ...');
 
